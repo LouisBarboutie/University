5a956ad236c715dfa6a7e31578ef62e2456bd8c2
TakashiSenpai <79802588+TakashiSenpai@users.noreply.github.com>
2021-03-10 14:29:20 +0100

    Delete yNextTemp.cpp

--- MainDiffEqFirstOrder/yNextTemp.cpp
+++
@@ -1,8 +0,0 @@
-#include <math.h>
-double yNextTemp(double x_i, double y_i, double h)
-{
-  double f (double, double);
-  double resultat;
-  resultat = y_i + h * f (x_i, y_i);
-  return (resultat);
-}
