f881957bcd8957d665cec2e41198a614ea34cd91
TakashiSenpai <79802588+TakashiSenpai@users.noreply.github.com>
2021-03-10 14:29:14 +0100

    Delete yNext.cpp

--- MainDiffEqFirstOrder/yNext.cpp
+++
@@ -1,8 +0,0 @@
-#include <math.h>
-double yNext(double yBarre, double x, double y, double h)
-{
-  double f (double, double);
-  double resultat;
-  resultat = y + h * 0.5 * (f (x, y) + f (x + h, yBarre));
-  return (resultat);
-}
