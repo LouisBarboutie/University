4fb7105b429c3f9cb41adea34f6fc8c0799e4771
TakashiSenpai <79802588+TakashiSenpai@users.noreply.github.com>
2021-03-31 15:57:57 +0200

    Add files via upload

---
+++ trapèzeApprox/f.cpp
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+#include <vector>
+#include <math.h>
+#include <iomanip>
+  
+double f( double x)
+{
+    double resf = pow(x,2);
+    return resf;
+}
