84492fb3e82d6e50551d40272d0af58d2b81fd78
TakashiSenpai <79802588+TakashiSenpai@users.noreply.github.com>
2022-09-22 12:47:01 +0200

    make shit work

--- Bruker/au_metabol_2.c
+++ Bruker/au_metabol_2.c
@@ -1,40 +1,67 @@
 /*
     Description:
+    This AU program performs automatic peak picking, for filtering out water signal.
+     With the input of ppm ranges, it looks for the peak and applies the correct offset frequency for the following experiments
 
     Author: Louis-Hendrik Barboutie
+    e-mail: louis.barboutie@gmail.com
+
+    Changelog
+    - 22/09/2022    created
 */
 
 AUERR = au_metabol_2(curdat);
 QUITMSG("--- AU program au_metabol_2 finished ---")
 
 int au_metabol_2(const char* curdat){
-    
-    char dataset_path[PATH_MAX];
-    int d_list[64];
-    char peak_freq[PATH_MAX];
-    char command_string[PATH_MAX];
-    char line[PATH_MAX];
+	int i = 0;
+    float H2O_peak_freq; 
+    char peaklist_path[PATH_MAX]; 
+    char dataset_name[PATH_MAX];
+    char line[PATH_MAX]; // string for reading the files
+    int exp_name[7] = { 1, 2, 10, 11, 12, 13, 100};
+    float F1P, F2P, MI; // limits and minimum intensity for the peak picking
     FILE *fptr;
 
-    CPR_exec("pps", WAIT_TERM)
-    CPR_exec("convertpeaklist peaklist", WAIT_TERM)
+    // get the peaklist path, with user input
+    strcpy(peaklist_path, "dataset_path");
+    GETSTRING("enter path to dataset (right click -> copy, remove blank space in the end!!):", peaklist_path)
+    strcat(peaklist_path, "/1/pdata/1/peaklist"); // path should now look like this: "dataset_path/1/pdata/1/peaklist"
 
-    strcpy(dataset_path, "dataset_path");
-    GETSTRING("enter path to dataset (right click -> copy):", dataset_path);
-    strcat(dataset_path, "/1/pdata/1/peaklist");
+    // peak picking parameters, user input
+    F1P = 5.1; // default values
+    F2P = 4.9;
+    MI = 5;
+    GETFLOAT("enter left limit for peak picking (ppm):", F1P) // user input values
+    GETFLOAT("enter right limit for peak picking (ppm):", F2P)
+    GETFLOAT("enter minimum intensity for peak picking:", MI)
+    STOREPAR("F1P", F1P) // store the parameters
+    STOREPAR("F2P", F2P)
+    STOREPAR("MI", MI)
 
-    fptr = fopen(dataset_path);
+	// do peak picking	
+    PPP
+
+    // read peaklist and find the peak frequency
+    fptr = fopen(peaklist_path, "r");
+    if (fptr == NULL){
+       Proc_err(DEF_ERR_OPT, "error opening file %s ", peaklist_path);
+       return EXIT_FAILURE;
+    }
     while (fgets(line, sizeof(line), fptr) != NULL){
-        if (sscanf(line, "%s", &peak_freq) == 1){
+        if (sscanf(line, "%f", &H2O_peak_freq) == 1){
             break;
         } 
     }
     fclose(fptr);
-    
-    strcpy(command_string, "o1 ");
-    strcat(command_string, peak_freq);
-
-    CPR_exec(command_string, WAIT_TERM)
+ 
+    // set the parameter  
+    TIMES(7) 
+    	REXPNO(exp_name[i])
+    	SETCURDATA
+    	STOREPAR("o1", H2O_peak_freq)
+    	i++;
+    END
 
     return EXIT_SUCCESS;
 }
